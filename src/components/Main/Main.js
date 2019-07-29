e269e764a92be9250cb1bd747650c4bf323167da
Franco Sammartino <franco.sammartino@synapse.com>
2019-07-29 17:41:17 -0300

    lunes95

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,71 +7,68 @@ import { getImg } from '../../utils/api.js';
 
 class Main extends Component {
  constructor() {
-  super()
-  this.state = {
-   topText: '',
-   botText: '',
-   allMemeImgs: [],
-   randomImg: ''
-  };
-
+    super()
+    this.state = {
+     topText: '',
+     botText: '',
+     allMemeImgs: [],
+     randomImg: ''
+    };
   this.handleClick = this.handleClick.bind(this);
   this.handleChange = this.handleChange.bind(this);
  }
  componentDidMount() {
-  getImg().then( data => {
-   this.setState({
-     allMemeImgs: data.data.memes
-   });
-    console.log(this.state.allMemeImgs)
-  })
+    getImg().then( data => {
+     this.setState({
+       allMemeImgs: data.data.memes
+     });
+    })
  }
  handleChange(e) {
-  const { name, value } = e.target;
-  this.setState({
-   [name]: value
-  })
+    const { name, value } = e.target;
+    this.setState({
+     [name]: value
+    })
  }
  handleClick(e) {
-  e.preventDefault();
-  const { allMemeImgs } = this.state;
-  const randNum = Math.floor(Math.random() * allMemeImgs.length );
-  const randMemeImg = allMemeImgs[randNum].url;
-  console.log(randMemeImg)
-  this.setState({
-    randomImg: randMemeImg
-  })
+    e.preventDefault();
+    const { allMemeImgs } = this.state;
+    const randNum = Math.floor(Math.random() * allMemeImgs.length );
+    const randMemeImg = allMemeImgs[randNum].url;
+    this.setState({
+      randomImg: randMemeImg
+    })
  }
  render() {
-  const { topText, botText, randomImg } = this.state;
-  return (
-    <div>
-      <form className="meme-form">
-       <label htmlFor="input">Top text:</label>
-        <input
-         type="text"
-         name="topText"
-         placeholder="Set Top Text"
-         value={topText}
-         onChange={this.handleChange}
-        />
-        <label htmlFor="input">Bottom text:</label>
-        <input
-         type="text"
-         name="botText"
-         placeholder="Set Bottom Text"
-         value={botText}
-         onChange={this.handleChange}
-        />
-        <button onClick={this.handleClick}>Generate</button>
-      </form>
-      <div className="meme">
-       <img src={randomImg} alt=""/>
-       <h2 className="top">{topText}</h2>
-       <h2 className="bot">{botText}</h2>
+    const { topText, botText, randomImg } = this.state;
+    return (
+      <div>
+        <form className="meme-form">
+         <label htmlFor="input">Top text:</label>
+          <input
+           type="text"
+           name="topText"
+           placeholder="Set Top Text"
+           value={(topText.upperCase)}
+           onChange={this.handleChange}
+          />
+          <label htmlFor="input">Bottom text:</label>
+          <input
+           type="text"
+           name="botText"
+           placeholder="Set Bottom Text"
+           value={botText}
+           onChange={this.handleChange}
+          />
+          <button onClick={this.handleClick}>Get Meme</button>
+        </form>
+        <div className="meme">
+         <img src={randomImg} alt=""/>
+         <h2 className="top">{topText}</h2>
+         <h2 className="bot">{botText}</h2>
+        </div>
       </div>
-    </div>
-   )
-  }
+    )
+ }
  }
 export default Main;
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,5 @@ import axios from 'axios'
 export async function getImg() {
  const data = await axios.get('https://api.imgflip.com/get_memes')
   .catch( error => console.log(error));
-  console.log('Api Call', data.data)
  return data.data;
 }
